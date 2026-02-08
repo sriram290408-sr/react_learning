f5a4cc84e12af5a171c0abdea1050d69c9e3664b
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-02-08 22:11:53 +0530

    folder path change

--- cart card/src/details.jsx
+++
@@ -1,7 +0,0 @@
-const products = [
-    {id:1, img: "./src/assets/1.png"},
-    {id:2, img: "./src/assets/2.png"},
-    {id:3, img: "./src/assets/3.png"}
-]
-
-export default products;
--- weekend task 1/src/product.jsx
+++
@@ -1,7 +0,0 @@
-const Product= [
-    {id:1, img: "src/assets/product1.png"},
-    {id:2, img: "src/assets/product2.png"},
-    {id:3, img: "src/assets/product3.png"}
-]
-
-export default Product;
